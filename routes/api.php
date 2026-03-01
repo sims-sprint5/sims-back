9196e1ad4c04f5a1420e15eaea9680ed72b18958
Iker Vericat <ikervericat@iesmontsia.org>
2026-03-01 22:06:44 +0100

    Fix: tenant routes

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-// ============================
-// RUTAS DE AUTENTICACIÓN
-// ============================
-Route::get('/ping', function() {
+Route::get('/ping', function () {
     return response()->json(['message' => 'API is working']);
 });
--- routes/tenant.php
+++ routes/tenant.php
@@ -29,7 +29,10 @@ Route::middleware([
     'api',
     InitializeTenancyBySubdomain::class,
     PreventAccessFromCentralDomains::class,
+
 ])->group(function () {
+
+
     Route::get('/', function () {
         return 'This is your multi-tenant application. The id of the current tenant is ' . tenant('id');
     });
