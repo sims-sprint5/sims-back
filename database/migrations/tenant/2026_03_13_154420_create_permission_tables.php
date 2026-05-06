8d57178d2feadc170178b2b4bac4d4d1027c9acb
morenobooop <llorenc.moreno@iesmontsia.org>
2026-05-06 18:56:30 +0200

    fix: add strict types declaration and refactor migration class structure

--- database/migrations/tenant/2026_03_13_154420_create_permission_tables.php
+++ database/migrations/tenant/2026_03_13_154420_create_permission_tables.php
@@ -1,10 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+final class CreatePermissionTables extends Migration
 {
     /**
      * Run the migrations.
@@ -138,4 +140,6 @@ return new class extends Migration
         Schema::dropIfExists($tableNames['roles']);
         Schema::dropIfExists($tableNames['permissions']);
     }
-};
+}
+
+return new CreatePermissionTables();
